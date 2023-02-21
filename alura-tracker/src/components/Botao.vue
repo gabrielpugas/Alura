d9e5b5e97d4c1140a6fd908c1cea83f4a8cafad1
Gabriel Pugas <gabriel.pg@lyncas.net>
2023-02-21 17:33:00 -0300

    Alura tracker, criando componentes

---
+++ alura-tracker/src/components/Botao.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button" >
+    <span class="icon">
+      <i class="fas fa-play"></i>
+    </span>
+    <span>play</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent ({
+    name: "Botao",
+    props: {
+
+    }
+})
+</script>
---
+++ alura-tracker/src/components/Formulario.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa."
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoFinalizarTemporizador="finalizarTarefa"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue"
+
+export default defineComponent({
+  name: "Formulario",
+  data () {
+    return {
+        descricao: ""
+    }
+  },
+  components: {
+    Temporizador
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number) : void {
+        console.log(this.descricao)
+        console.log(tempoDecorrido)
+        this.descricao = ""
+    }
+  }
+})
+</script>
